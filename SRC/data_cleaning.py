a4375b636fa85e6997dc33839306dfa1a543a74e
kaveribartakke2702 <kaveribartakke@gmail.com>
2026-06-21 00:54:44 +0530

    Update and rename src to SRC/data_cleaning.py
    Added data cleaning script

---
+++ SRC/data_cleaning.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Load raw dataset
+data = pd.read_csv("../data/student_raw_data.csv")
+
+# Display first 5 rows
+print(data.head())
+
+# Check missing values
+print("Missing Values:")
+print(data.isnull().sum())
+
+# Remove duplicate records
+data = data.drop_duplicates()
+
+# Save cleaned dataset
+data.to_csv("../data/student_cleaned_data.csv", index=False)
+
+print("Data cleaning completed successfully")
